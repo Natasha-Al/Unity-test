adf91d87b1cf0bd544d9bdfdc56e985ad4b21da2
Benjamin Gulliver <fatherbenooo@gmail.com>
2019-07-01 11:14:45 +1000

    Added a new scene in which I tested some collision detection.

---
+++ Assets/Scenes/Trigger Collisions/TriggerDetectors.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetectors : MonoBehaviour
+{
+    public GameObject theLight;
+
+    public bool lightIsOn = true;
+
+    public GameObject leCube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (lightIsOn == true)
+        {
+            theLight.SetActive(true);
+        }
+        else
+        {
+            theLight.SetActive(false);
+        }
+	}
+
+    void OnTriggerEnter()
+    {
+        print("yeets");
+        lightIsOn = !lightIsOn;
+        Destroy(leCube);
+    }
+
+    void OnTriggerExit()
+    {
+        print("poo");
+        lightIsOn = !lightIsOn;
+    }
+}
